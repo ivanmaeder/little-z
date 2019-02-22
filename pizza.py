a9c297ebbdadc3081917408a6b462efccbb8ece9
Ivan Maeder <ivan.maeder@gmail.com>
2019-02-22 15:59:27 +0100

    Allow different pizzeria locations

--- pizza.py
+++ pizza.py
@@ -15,25 +15,12 @@ def create_city(city_dimension):
     return [[0 for x in range(city_dimension)] for y in range(city_dimension)]
 
 def pizzeria_area(x, y, k):
-    return [(x, y) for x in range(-k, k + 1) for y in range(-k, k + 1) if abs(x) + abs(y) <= k and abs(x) + abs(y) >= -k]
-#    result = []
-#    if (k == 0):
-#        return [(x, y)]
-#    if (k == 1):
-#        result.append((x, y + k))
-#        result.append((x - k, y))
-#        result.append((x, y))
-#        result.append((x + k, y))
-#        result.append((x, y - k))
-##[[x,y,z] for x in range(X) for y in range(Y) for z in range(Z) if x+y+z != N]
-##[[x, y] for x in range(-3, 3) for y in range(-3, 3) if x + y < 3] <<<<< not bad
-#    if (k == 2):
-#        result.append((x, y + k))
-#        result.append((x))
-#        
-#        result.extend([(x - 1, y + 1), (x, y + 1), (x + 1, y + 1), (x - 2, y), (x - 1, y), (x, y), (x + 1, y), (x + 2, y), (x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x, y - k)])
-#
-#    return result
+    return [
+        (potential_x, potential_y)
+        for potential_x in range(x - k, x + k + 1)
+        for potential_y in range(y - k, y + k + 1)
+        if (abs(x - potential_x) + abs(y - potential_y) <= k)
+    ]
 
 def main():
     for line in fileinput.input():
--- test_pizza_area.py
+++ test_pizza_area.py
@@ -1,7 +1,9 @@
 from pizza import pizzeria_area
 
 def test_size_0():
-    assert [(0, 0)] == pizzeria_area(0, 0, 0)
+    expected = [(0, 0)]
+    
+    assert expected == pizzeria_area(0, 0, 0)
 
 def test_size_1():
     expected = [         (-1, 0),
@@ -29,3 +31,17 @@ def test_size_3():
                                              ( 3, 0)                           ]
 
     assert expected == pizzeria_area(0, 0, 3)
+
+def test_size_0_different_location():
+    expected = [(2, 7)]
+
+    assert expected == pizzeria_area(2, 7, 0)
+
+def test_size_2_different_location():
+    expected = [                (1, 9),
+                        (2, 8), (2, 9), (2, 10),
+                (3, 7), (3, 8), (3, 9), (3, 10), (3, 11),
+                        (4, 8), (4, 9), (4, 10),
+                                (5, 9)                   ]
+
+    assert expected == pizzeria_area(3, 9, 2)
